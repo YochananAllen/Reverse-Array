1d942aace2674803cfa271a1499e38a921444634
yochanan allen <b2allen1993@gmail.com>
2025-01-30 07:36:05 -0500

    Add project files.

---
+++ Reverse Array.cpp
@@ -0,0 +1,59 @@
+// Reverse Array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Yochanan Allen 
+//This program populates an array with values from 1 to 10, then calls a function to reverse the
+//array.The program then displays both the original and reversed arrays.
+
+
+#include <iostream>  
+
+using namespace std;  
+
+
+void ReverseArray(const int arr[], int reversedArr[], int size) {
+    
+    for (int i = 0; i < size; i++) {
+        reversedArr[i] = arr[size - 1 - i];  
+    }
+}
+
+
+void print(const int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";  
+    }
+    cout << endl;  
+}
+
+int main() {
+    
+    int originalArray[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int size = 10;  
+
+    
+    int reversedArray[10];  
+
+    
+    cout << "Original array:" << endl;
+    print(originalArray, size);  
+
+    
+    ReverseArray(originalArray, reversedArray, size);  
+
+    
+    cout << "Reversed array:" << endl;
+    print(reversedArray, size);  
+
+    return 0;  
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
